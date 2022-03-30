85ff0b37d30f1295964be0740669f79f250c5827
wesleytw <wesliutw@gmail.com>
2022-03-30 23:31:43 +0800

    moralis added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,54 @@
 import React from 'react';
 import { Navbar, Welcome, Transactions, Loader, Services, Footer } from './components';
-// import { useMoralis } from "react-moralis";
+import { useMoralis } from "react-moralis";
 
-const  App = () => {
+// const  App = () => {
 
-  // const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
-  // const login = async () => {
-  //   if (!isAuthenticated) {
+//   // gradient-bg-welcome
+//   return (
+//     <div className="min-h-screen">
+//       <div className="gradient-bg-welcome">
+//       <Navbar />
+//       <Welcome />
+//       </div>
+//       <Welcome />
+//       <Loader />
+//       {/* <div class="sticky inset-x-0 top-0 left-0 bg-gray-400 py-5">Menu</div> */}
+//       <Loader />
+//       <Loader />
+//     </div>
+//   );
+// }
+// export default App
 
-  //     await authenticate({signingMessage: "Log in using Moralis" })
-  //       .then(function (user) {
-  //         console.log("logged in user:", user);
-  //         // console.log(user!.get("ethAddress"));
-  //       })
-  //       .catch(function (error) {
-  //         console.log(error);
-  //       });
-  //   }
-  // }
 
-  // const logOut = async () => {
-  //   await logout();
-  //   console.log("logged out");
-  // }
+import './App.css';
 
-  // gradient-bg-welcome
-  return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-      <Navbar />
-      <Welcome />
-      </div>
-      <Welcome />
-      <Loader />
-      {/* <div class="sticky inset-x-0 top-0 left-0 bg-gray-400 py-5">Menu</div> */}
-      <Loader />
-      <Loader />
-    </div>
-  );
+function App() {
+    const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+    const login = async () => {
+        if (!isAuthenticated) {
+            await authenticate({ signingMessage: "Log in using Moralis" })
+                .then(function (user) {
+                console.log("logged in user:", user);
+                console.log(user.get("ethAddress"));
+            })
+                .catch(function (error) {
+                console.log(error);
+            });
+        }
+    };
+    const logOut = async () => {
+        await logout();
+        console.log("logged out");
+    };
+    return (<div>
+      <h1>Moralis Hello World!</h1>
+      <button onClick={login}>Moralis Metamask Login</button>
+      <button onClick={logOut} disabled={isAuthenticating}>Logout</button>
+    </div>);
 }
-export default App
-
+export default App;
 
 
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import reportWebVitals from "./reportWebVitals";
-// import { MoralisProvider } from "react-moralis";
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
-  // <React.StrictMode>
-  // < MoralisProvider serverUrl="https://bdyir3ngauv6.usemoralis.com:2053/server" appId="ut3Icy6uM55LLdHwrW0q2dCrtLqyJYkjCHbGxoJu">
-    <App />,
-  // </ MoralisProvider > 
-  // </React.StrictMode>,
+  < MoralisProvider serverUrl="https://bdyir3ngauv6.usemoralis.com:2053/server" appId="ut3Icy6uM55LLdHwrW0q2dCrtLqyJYkjCHbGxoJu">
+    <App />
+  </ MoralisProvider > ,
   document.getElementById('root')
 );
